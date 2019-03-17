cb61ebe4f81d2e804f8fe643955b2fcb5b03585d
svensone <svenson.ertel@gmail.com>
2019-03-17 10:52:43 +0800

    signup and signin components created

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,21 +1,39 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
- // Initialize Firebase ( using env. file in root - not sure if working, check gitignore)
-// optional with firebase Project panti-prod - config in new env.production - see tutorial) 
- 
- const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  };
+// Initialize Firebase ( using env. file in root - not sure if working, check gitignore)
+// optional with firebase Project panti-prod - config in new env.production - see tutorial)
 
-  class Firebase {
-      constructor(){
-          app.initializeApp(config);
-      }
+var config = {
+  apiKey: "AIzaSyASqvoZ3hGYNnp5_BTjHm88zZAHzFLuYCA",
+  authDomain: "panti-3a46f.firebaseapp.com",
+  databaseURL: "https://panti-3a46f.firebaseio.com",
+  projectId: "panti-3a46f",
+  storageBucket: "panti-3a46f.appspot.com",
+  messagingSenderId: "1039443157872"
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+
+    this.auth = app.auth();
   }
 
-  export default Firebase;
+  // **** Auth API ***
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
