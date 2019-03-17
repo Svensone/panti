b95809ff8534031b2b6e5c28e97dfca70a88a77f
svensone <svenson.ertel@gmail.com>
2019-03-17 10:53:29 +0800

    firebase .env api-key update

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 // Initialize Firebase ( using env. file in root - not sure if working, check gitignore)
 // optional with firebase Project panti-prod - config in new env.production - see tutorial)
 
-var config = {
-  apiKey: "AIzaSyASqvoZ3hGYNnp5_BTjHm88zZAHzFLuYCA",
-  authDomain: "panti-3a46f.firebaseapp.com",
-  databaseURL: "https://panti-3a46f.firebaseio.com",
-  projectId: "panti-3a46f",
-  storageBucket: "panti-3a46f.appspot.com",
-  messagingSenderId: "1039443157872"
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 class Firebase {
